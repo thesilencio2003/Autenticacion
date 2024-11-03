add325bdcab093193ad5467a965d83f60bc092ee
Alejandro <alejohola24@gmail.com>
2024-11-02 23:44:18 -0500

    initial pages

---
+++ Pages/Student/Student.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Autenticacion.Pages.Student
+{
+    public class StudentModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Subject/Subject.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Autenticacion.Pages.Subject
+{
+    public class SubjectModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
